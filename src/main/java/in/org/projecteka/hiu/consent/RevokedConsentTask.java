a8afbf78cb7dd6855b48ea70335b78f6b641a515
MOHANKUMAR T <mohan13081999@gmail.com>
2025-02-04 16:12:03 +0530

    BAH-4163 | Refactor. Extract duplicate methods to the base class for ConsentTask

--- src/main/java/in/org/projecteka/hiu/consent/RevokedConsentTask.java
+++ src/main/java/in/org/projecteka/hiu/consent/RevokedConsentTask.java
@@ -1,30 +1,22 @@
 package in.org.projecteka.hiu.consent;
 
 import in.org.projecteka.hiu.clients.GatewayServiceClient;
-import in.org.projecteka.hiu.common.GatewayResponse;
 import in.org.projecteka.hiu.consent.model.ConsentArtefact;
 import in.org.projecteka.hiu.consent.model.ConsentArtefactReference;
 import in.org.projecteka.hiu.consent.model.ConsentNotification;
 import in.org.projecteka.hiu.consent.model.ConsentStatus;
-import in.org.projecteka.hiu.consent.model.consentmanager.ConsentAcknowledgement;
-import in.org.projecteka.hiu.consent.model.consentmanager.ConsentOnNotifyRequest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import static in.org.projecteka.hiu.ClientError.consentArtefactNotFound;
-import static in.org.projecteka.hiu.common.Constants.getCmSuffix;
 import static in.org.projecteka.hiu.consent.model.ConsentStatus.EXPIRED;
 import static in.org.projecteka.hiu.consent.model.ConsentStatus.REVOKED;
-import static in.org.projecteka.hiu.consent.model.consentmanager.ConsentAcknowledgementStatus.OK;
 
 public class RevokedConsentTask extends ConsentTask {
-    private static final String CONSENT_REVOKE_MESSAGE = "Consent is revoked";
     private final HealthInformationPublisher healthInformationPublisher;
     private final GatewayServiceClient gatewayServiceClient;
 
@@ -54,24 +46,6 @@ public class RevokedConsentTask extends ConsentTask {
                 .then(Mono.defer(() -> Flux.fromIterable(consentNotification.getConsentArtefacts())
                         .flatMap(reference -> processArtefactReference(reference, timeStamp))
                         .then()));
-    }
-
-    private ConsentOnNotifyRequest buildConsentOnNotifyRequest(List<ConsentArtefact> consentArtefacts,UUID responseRequestId) {
-        var consentArtefactRequest = ConsentOnNotifyRequest
-                .builder();
-        var acknowledgements = new ArrayList<ConsentAcknowledgement>();
-
-        for (ConsentArtefact consentArtefact : consentArtefacts) {
-            acknowledgements.add(ConsentAcknowledgement.builder().consentId(consentArtefact.getConsentId()).status(OK).build());
-        }
-        GatewayResponse gatewayResponse = new GatewayResponse(responseRequestId.toString());
-        consentArtefactRequest.response(gatewayResponse).build();
-        return consentArtefactRequest.acknowledgement(acknowledgements).build();
-    }
-
-    private String getCmSuffixFromArtefact(List<ConsentArtefact> consentArtefacts) {
-        ConsentArtefact consentArtefact = consentArtefacts.get(0);
-        return getCmSuffix(consentArtefact.getPatient().getId());
     }
 
     private Mono<List<ConsentArtefact>> validateConsents(List<ConsentArtefactReference> consentArtefacts) {
